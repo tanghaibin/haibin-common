8a1bf397d686c303708439d72698fc7344140771
tanghaibin <tanghaibinhaohan@163.com>
2018-06-21 17:40:32 +0800

    refactor

--- src/main/java/com/haibin/common/aspect/LogAspect.java
+++ src/main/java/com/haibin/common/aspect/LogAspect.java
@@ -2,9 +2,11 @@ package com.haibin.common.aspect;
 
 import com.haibin.common.annotation.OutLog;
 import com.haibin.common.util.JsonUtil;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import java.lang.reflect.Method;
 
 /**
@@ -17,34 +19,40 @@ public class LogAspect {
 
     private static final Logger LOG = LoggerFactory.getLogger(LogAspect.class);
 
-    public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
-        try {
-            if(!LOG.isDebugEnabled()) {
-                return joinPoint.proceed(joinPoint.getArgs());
-            }
-            Method targetMethod = getTargetMethod(joinPoint);
-            if (targetMethod == null) {
-                LOG.debug("未获取到目标方法");
-                return joinPoint.proceed(joinPoint.getArgs());
-            }
-            OutLog outLog = targetMethod.getAnnotation(OutLog.class);
-            if (outLog == null) {
-                return joinPoint.proceed(joinPoint.getArgs());
-            }
-            int[] index = outLog.index();
-            final String[] desc = outLog.prefix();
-            Object[] args = joinPoint.getArgs();
-            int prefixIndex = 0;
-            for (int i : index) {
-                LOG.debug("{}:{}", desc[prefixIndex++], JsonUtil.obj2Json(args[i]));
-            }
-            return joinPoint.proceed(joinPoint.getArgs());
-        } catch (Throwable e) {
-            return joinPoint.proceed(joinPoint.getArgs());
+    /**
+     * 该方法配置为前置通知
+     * @param joinPoint
+     * @throws Throwable
+     */
+    public void log(JoinPoint joinPoint) throws Throwable {
+
+        if (!LOG.isDebugEnabled()) {
+            return;
+        }
+        Method targetMethod = getTargetMethod(joinPoint);
+        if (targetMethod == null) {
+            LOG.debug("未获取到目标方法");
+            return;
+        }
+        OutLog outLog = targetMethod.getAnnotation(OutLog.class);
+        if (outLog == null) {
+            return;
+        }
+        int[] index = outLog.index();
+        final String[] desc = outLog.prefix();
+        Object[] args = joinPoint.getArgs();
+        int prefixIndex = 0;
+        final String reference = getReference(targetMethod);
+        for (int i : index) {
+            LOG.debug("{}:{}:{}", reference, desc[prefixIndex++], JsonUtil.obj2Json(args[i]));
         }
     }
 
-    private Method getTargetMethod(ProceedingJoinPoint point) {
+    private String getReference(Method method) {
+        return method.getDeclaringClass().getSimpleName() + "#" + method.getName();
+    }
+
+    private Method getTargetMethod(JoinPoint point) {
         Object[] args = point.getArgs();
         Class<?>[] argTypes = new Class[point.getArgs().length];
         for (int i = 0; i < args.length; i++) {
