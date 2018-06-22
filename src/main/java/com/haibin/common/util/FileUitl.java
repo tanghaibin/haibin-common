5aca68c59cf73581b388a5d3965e447b38a73c8e
tanghaibin <tanghaibinhaohan@163.com>
2018-06-22 10:35:46 +0800

    refactor

--- src/main/java/com/haibin/common/util/FileUitl.java
+++ src/main/java/com/haibin/common/util/FileUitl.java
@@ -4,6 +4,7 @@ import com.haibin.common.exception.BizException;
 
 import javax.imageio.stream.FileImageOutputStream;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 
 /**
@@ -20,7 +21,8 @@ public class FileUitl {
      * @param path 路径
      * @return
      */
-    public static String newImg(InputStream inputStream, final String path, final String suffix) {
+    public static String createImg(InputStream inputStream, final String path, final String suffix) {
+        FileImageOutputStream fileImageOutputStream = null;
         try {
             File dir = new File(path);
             if (!dir.exists()) {
@@ -29,7 +31,7 @@ public class FileUitl {
             String fileName = StringUtil.getUUID() + suffix;
             File newFile = new File(path + fileName);
             newFile.createNewFile();
-            FileImageOutputStream fileImageOutputStream = new FileImageOutputStream(newFile);
+            fileImageOutputStream = new FileImageOutputStream(newFile);
             byte[] buf = new byte[1024 * 1024];
             while (inputStream.read(buf) != -1) {
                 fileImageOutputStream.write(buf, 0, buf.length);
@@ -37,6 +39,14 @@ public class FileUitl {
             return fileName;
         }catch (Exception e) {
             throw new BizException("新建图片失败", e);
+        } finally {
+            if(fileImageOutputStream != null) {
+                try {
+                    fileImageOutputStream.close();
+                } catch (IOException e) {
+                    throw new BizException("新建图片失败", e);
+                }
+            }
         }
     }
 }
