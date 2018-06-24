51ce5d14104beb7dc826d4d441ea1718f6e3741b
tanghaibin <tanghaibinhaohan@163.com>
2018-06-24 12:12:07 +0800

    refactor

---
+++ src/main/java/com/haibin/common/constants/CommonConstant.java
@@ -0,0 +1,14 @@
+package com.haibin.common.constants;
+
+/**
+ * 基础常量
+ *
+ * @author haibin.tang
+ * @create 2018-06-24 上午11:51
+ **/
+public interface CommonConstant {
+
+    String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+    String DATE_FORMAT = "yyyy-MM-dd";
+}
